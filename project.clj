c465e52bcb6b2b78306cf689e00a95e5331d1af4
bilus <gyamtso@gmail.com>
2015-09-03 11:29:55 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bilus/crypto "0.1.2"
+(defproject bilus/crypto "0.1.3"
   :description  "Small Clojure lib to provide basic GPG keypair generation, encryption & decryption facilities"
   :url          "https://github.com/thi-ng/crypto"
   :license      {:name "Apache Software License 2.0"
@@ -7,5 +7,5 @@
   :scm          {:name "git"
                  :url "git@github.com:thi-ng/crypto.git"}
   :dependencies [[org.clojure/clojure "1.6.0"]]                 
-  :profiles {:provided {:dependencies [[org.bouncycastle/bcprov-jdk15on "1.52"]]}
-             :dev      {:dependencies [[org.bouncycastle/bcprov-jdk15on "1.52"]]}})
+  :profiles {:provided {:dependencies [[org.bouncycastle/bcpg-jdk15on "1.52"]]}
+             :dev      {:dependencies [[org.bouncycastle/bcpg-jdk15on "1.52"]]}})
